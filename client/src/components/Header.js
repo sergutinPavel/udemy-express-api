4c108b66cbc8791763d602f15d744606512d435a
pavel <pavel.sergutin@cronix.ms>
2017-12-06 13:17:06 +0300

    fetch user action

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
           <a className="left brand-logo">Emaily</a>
           <ul className="right">
             <li>
-              <a>Login With Google</a>
+              <a href="/auth/google">Login With Google</a>
             </li>
           </ul>
         </div>
